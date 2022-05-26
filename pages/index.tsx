7b0f1229c22ca51fcfae1d5117c93c2b0e38f40b
Truongnam <hoangnamqt.2001@gmail.com>
2022-05-26 23:22:30 +0700

    1 day ui

---
+++ pages/index.tsx
@@ -0,0 +1,87 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Banner from '../components/Banner'
+import Header from '../components/Header'
+import requests from '../utils/requests'
+import { Movie } from '../typings'
+import Rows from '../components/Rows'
+interface Props {
+  netflixOriginals: Movie[]
+  trendingNow: Movie[]
+  topRated: Movie[]
+  actionMovies: Movie[]
+  comedyMovies: Movie[]
+  horrorMovies: Movie[]
+  romanceMovies: Movie[]
+  documentaries: Movie[]
+}
+const Home = ({
+  netflixOriginals,
+  trendingNow,
+  topRated,
+  actionMovies,
+  comedyMovies,
+  horrorMovies,
+  romanceMovies,
+  documentaries,
+}: Props) => {
+  return (
+    <div className="relative h-screen bg-gradient-to-b  lg:h-[140vh]">
+      <Head>
+        <title>Home - Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <Banner netflixOriginals={netflixOriginals} />
+      <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
+        <section className="md:space-y-24">
+          <Rows title="Trending Now" movies={trendingNow} />
+          <Rows title="Top Rated" movies={topRated} />
+          <Rows title="Action Thrillers" movies={actionMovies} />
+          {/* My List  Component*/}
+          <Rows title="Comedies" movies={comedyMovies} />
+          <Rows title="Scary Movies" movies={horrorMovies} />
+          <Rows title="Romance Movies" movies={romanceMovies} />
+          <Rows title="Documentaries" movies={documentaries} />
+        </section>
+      </main>
+
+      {/* Modal */}
+    </div>
+  )
+}
+
+export default Home
+export const getServerSideProps = async () => {
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
+    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+    fetch(requests.fetchTrending).then((res) => res.json()),
+    fetch(requests.fetchTopRated).then((res) => res.json()),
+    fetch(requests.fetchActionMovies).then((res) => res.json()),
+    fetch(requests.fetchComedyMovies).then((res) => res.json()),
+    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+    fetch(requests.fetchDocumentaries).then((res) => res.json()),
+  ])
+  return {
+    props: {
+      netflixOriginals: netflixOriginals.results,
+      trendingNow: trendingNow.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentaries: documentaries.results,
+    },
+  }
+}
